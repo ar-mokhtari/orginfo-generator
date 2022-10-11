68b189effeabe0ab0318b1a05e23042917763e65
HQ2-AlirezaMokhtariG <neatland@gmail.com>
2022-10-11 15:52:54 +0330

    init

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package generator
 
 import (
+	"fmt"
 	"os"
 )
 
 func New() {
+	//main root
+	fmt.Sprintf("root is: %s", os.Args[1])
 	//prepare command(s) with flag(s)
 	flagErr := rootCommand(os.Args[1:])
 	if flagErr != nil {
