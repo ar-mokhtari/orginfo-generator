eb7727238028877212fc5924426a9af64321edb2
Alireza Mokhtari G <ar.mokhtari.g@gmail.com>
2022-10-22 12:37:14 +0330

    go fmt->add to project_creator.sh

--- project_creator.sh
+++ project_creator.sh
@@ -13,61 +13,54 @@ echo "--------cd ~/go/src---------\n";
 mkdir -p -m777 $project_name/cli/generator;
 echo "--------mkdir -p -m777 $project_name/cli/generator---------\n";
 echo "package main
-
 import \"github.com/ar-mokhtari/orginfo-generator\"
-
 func main() {
-   generator.New()
+generator.New()
 }
-
 " > $project_name/cli/generator/main.go;
+go fmt $project_name/cli/generator/main.go;
 echo "--------$project_name/cli/generator/main.go;---------\n";
 echo "package main
-
 import (
-	\"$project_name/adapter/storage\"
-	\"$project_name/delivery/http\"
+\"$project_name/adapter/storage\"
+\"$project_name/delivery/http\"
 )
-
 func init() {
-	//initialise D.B.M.S. (Database management system)
-	storage.Init()
-	//initialise new Echo (web framework)
-	http.Init()
+//initialise D.B.M.S. (Database management system)
+storage.Init()
+//initialise new Echo (web framework)
+http.Init()
 }
-
 func main() {
-
 }" > $project_name/main.go;
+go fmt $project_name/main.go;
 echo "-------- $project_name/main.go;---------\n";
 echo "module $project_name
-
 go 1.19
-
 require (
-	github.com/ar-mokhtari/orginfo-generator v0.0.0-20221011095741-3289a676bde8
-	github.com/dlclark/regexp2 v1.7.0
-	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
-	github.com/labstack/echo/v4 v4.9.0
-	golang.org/x/crypto v0.0.0-20221010152910-d6f0a8c073c2
-	gorm.io/driver/mysql v1.3.6
-	gorm.io/gorm v1.23.8
+github.com/ar-mokhtari/orginfo-generator v0.0.0-20221011095741-3289a676bde8
+github.com/dlclark/regexp2 v1.7.0
+github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+github.com/labstack/echo/v4 v4.9.0
+golang.org/x/crypto v0.0.0-20221010152910-d6f0a8c073c2
+gorm.io/driver/mysql v1.3.6
+gorm.io/gorm v1.23.8
 )
-
 require (
-	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/labstack/gommon v0.3.1 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
-	golang.org/x/text v0.3.7 // indirect
+github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+github.com/go-sql-driver/mysql v1.6.0 // indirect
+github.com/jinzhu/inflection v1.0.0 // indirect
+github.com/jinzhu/now v1.1.5 // indirect
+github.com/labstack/gommon v0.3.1 // indirect
+github.com/mattn/go-colorable v0.1.11 // indirect
+github.com/mattn/go-isatty v0.0.14 // indirect
+github.com/valyala/bytebufferpool v1.0.0 // indirect
+github.com/valyala/fasttemplate v1.2.1 // indirect
+golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+golang.org/x/text v0.3.7 // indirect
 )" > $project_name/go.mod && cd $project_name/cli/generator;
+go fmt $project_name/go.mod && cd $project_name/cli/generator;
 echo "--------$project_name/go.mod && cd $project_name/cli/generator---------\n";
 go mod tidy;
 echo "--------go mod tidy---------\n";
