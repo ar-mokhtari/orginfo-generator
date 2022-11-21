432706e75b139a193eb813b651b5882503d6b3ab
Alireza Mokhtari G <ar.mokhtari.g@gmail.com>
2022-11-21 10:40:45 +0330

    debug->env.MainRepositoryPath

--- main.go
+++ main.go
@@ -20,12 +20,13 @@ func New() {
 	if err != nil {
 		fmt.Printf("error: %s", err.Error())
 	}
-	spilitedPwd := strings.Split(pwd, "/")
-	mainPathSlice := spilitedPwd[:6]
-	repo, repoErr := ioutil.ReadFile(strings.Join(spilitedPwd[:6], "/") + "/go.mod")
+	splitPwd := strings.Split(pwd, "/")
+	mainPathSlice := splitPwd[:6]
+	fmt.Println(mainPathSlice)
+	repo, repoErr := ioutil.ReadFile(strings.Join(splitPwd[:6], "/") + "/go.mod")
 	if repoErr != nil {
 		fmt.Println(repoErr)
-		fmt.Println(strings.Join(spilitedPwd[:6], "/") + "go.mod")
+		fmt.Println(strings.Join(splitPwd[:6], "/") + "/go.mod")
 	}
 	repoContent := string(repo)
 	lines := strings.Split(repoContent, "\n")
@@ -33,7 +34,7 @@ func New() {
 	finalRepo := bytes.Replace([]byte(firstLine), []byte("module "), []byte(""), -1)
 	env.MainRepositoryPath = string(finalRepo)
 	if env.MainRepositoryPath == "" {
-		env.MainRepositoryPath = strings.Join(spilitedPwd[5:6], "/")
+		env.MainRepositoryPath = strings.Join(splitPwd[5:6], "/")
 	}
 	env.MainPath = strings.Join(mainPathSlice, "/") + "/"
 	//-------find project path---------
